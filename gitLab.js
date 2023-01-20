dbd1ee73581dc5a02334081c75209cc772ebe71d
hughbowie@me.com <hughbowie@me.com>
2023-01-20 09:40:24 -0500

    update

--- gitLab.js
+++ gitLab.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const axios = require('axios');
-const sinceDate = process.argv[2]; // YYYY-MM-DDTHH:MM:SSZ 2022-12-01
+const afterDate = process.argv[2]; // 2022-12-08
 const authAPIKey = process.env.authAPIKey;
 const user = process.env.user;
 const userId = process.env.userId;
 
-const GITLAB_BASE_URL = 'https://gitlab.com/api/v4';
+const GITLAB_BASE_URL = `https://gitlab.com/api/v4/users/9827304/events?action=pushed&after=1&per_page=1000`;
 
 
-async function getCommits(authAPIKey, user, sinceDate) {
+async function getCommits(authAPIKey, user, afterDate) {
 	try {
 		// Set up headers for request with API key for authentication
 		const headers = {
@@ -18,25 +18,73 @@ async function getCommits(authAPIKey, user, sinceDate) {
 		// Set up query params to filter by user and since date
 		const params = {
 			username: user,
-			since: sinceDate
+			since: afterDate
 		};
 
 		// Make request to GitLab API to get all commits
-		const response = await axios.get('https://gitlab.com/api/v4/commits', { headers, params });
 
-		// Return array of commits with stats
-		return response.data.map(commit => ({
-			id: commit.id,
-			message: commit.message,
-			additions: commit.stats.additions,
-			deletions: commit.stats.deletions,
-			total: commit.stats.total
+		const response = await axios.get(`https://gitlab.com/api/v4/users/9827304/events?action=pushed,commented&after=${afterDate}&per_page=1000`, { headers });
+
+		//const events = await JSON.stringify(response.data);
+
+		const allEvents = await response.data.map(events => ({
+
+			id: events.id,
+			action: events.action_name, //pushed or commented on
+			date: events.created_at,
+			targetTitle: events.target_title,
+
 		}));
-	} catch (error) {
+
+		if (allEvents.action_name === 'pushed to') {
+			const pushvents = await allEvents.map(push => ({
+				id: push.id,
+				date: push.created_at,
+				commitTitle: push.push_data.commit_title,
+
+			}));
+			console.table(pushvents);
+		}
+		//const commentEvents = await allEvents.filter(allEvents.action_name === 'commented on');
+
+
+		//console.table(commentEvents);
+
+
+
+		// const events = await response.data.map(event => ({
+		// 	id: event.id,
+		// 	projectId: event.project_id, //13016320
+		// 	action: event.action_name //pushed or commented on
+		// 	/*pushes, comments */
+		// }));
+
+		// const pushes = await events.filter(events.action_name === 'pushed to')
+		// pushes.map(push => ({
+		// 	Issue: push.push_data.commit_title,/*.match(/(\#\n+)/)*/
+		// 	Message: push.push_data.commit_title,/*.replace(/(\#\n+)/, '').replace(/(\|[iov]+)/g, '')*/
+		// 	Hours: push.push_data.commit_title,/*.match(/(\|[iov]+)/g),*/
+		// }));
+
+		// const comments = await events.filter(events.action_name === 'commented on');
+		// comments.map(comment => ({
+		// 	project_id: comment.project_id,
+		// 	target_title: comment.target_title,
+		// 	wfNo: comment.target_title/*.match(/([wf#]\n(6))/g)*/,
+		// 	Date: comment.created_at.toLocaleString(),
+		// }));
+
+
+		// return [events, comments, pushes];
+	}
+
+	catch (error) {
 		console.error(error);
 	}
 }
 
 // Example usage
-getCommits(authAPIKey, user, sinceDate)
-	.then(commits => console.log(commits));
+getCommits(authAPIKey, user, afterDate)
+	//.then(log => console.table(log));
+	// .then(comments => console.table(comments))
+	// .then(pushes => console.table(pushes));
--- sacks.js
+++ sacks.js
@@ -3,49 +3,114 @@ const fs = require('fs');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const { r, log, clear, checkCookie, device, r15, r23 } = require('./src/helpers');
-clear();
+const { r, log, clear, clearCookies, setEmailCookie, device, r15, r23 } = require('./src/helpers');
+//clear();
+//clearCookies();
 
 (async () => {
 	try {
-
-		//••••initialize '--start-in-incognito',
-		const browser = await puppeteer.launch({ headless: false, args: ['--window-size=1920,1047', '--window-position=0,0'] }); //////// executablePath: process.env.XPTH, userDataDir: process.env.USDD, slowMo: 100  ♻♻♻♻♻♻♻♻♻♻
+		//----initialize
+		/// options -> executablePath: process.env.XPTH, userDataDir: process.env.USDD, slowMo: 100  ♻♻♻♻♻♻♻♻♻♻
+		const browser = await puppeteer.launch({ headless: false, args: ['--window-size=1920,1047', '--start-in-incognito', '--window-position=0,0'] });
 		const page = await browser.newPage();
-		await page.goto('https://gitlab.com', { waitUntil: 'networkidle0' });
-		await page.waitForTimeout(1111);
+		//const emailCookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8'));
+		//await page.setCookie(emailCookies);
 
-		await page.goto('https://gitlab.com/users/sign_in', { waitUntil: 'networkidle0' });
+		await page.goto('https://www.saksoff5th.com/on/demandware.store/Sites-SaksOff5th-Site/en_US/EmailSubscribe-SetDeclineSubscriptionCookie', { waitUntil: 'networkidle0' });
+		await page.waitForTimeout(r15);
 
-		//••••- login submit
-		await page.waitForSelector("#user_login", { visible: true });
-		await page.type("#user_login", process.env.GITUSR);
-		await page.waitForTimeout(111);
-		await page.type("#user_password", process.env.GITPW);
-		await page.waitForTimeout(111);
-		await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), page.click('[data-testid="sign-in-button"]')]);
+		await page.goto('https://www.saksoff5th.com/search?q=Ben%20Sherman&prefn1=refinementProductType&prefv1=Casual%20Button-Down%20Shirts%7CShorts%7CT-Shirts&prefn2=sizeRefinement&prefv2=34%7CX-Large%7CLarge%7C33%7C36&srule=newest');
 
-		//•••• goto user activity
-		await page.goto('https://gitlab.com/users/' + process.env.GITUSR + '/activity', { waitUntil: 'networkidle2' });
-		for (let i = 0; i < 50; i++) {
+		//const popupModal = await page.$('div#popup-modal');
+		await page.waitForSelector('h1.search-results-header');
+
+		for (let i = 0; i < 10; i++) {
 			await page.keyboard.press('PageDown');
-			await page.waitForTimeout(555);
+			await page.waitForTimeout(r(111, 222));
 		}
 
-		//•••• get all ccommit rows
+		const output = [];
+		const benShermanProducts = await page.$$('div.product.bfx-disable-product.standard');
+		for (let i = 0; i < benShermanProducts.length; i++) {
+
+			const benShermanPhotos = await page.$$eval('div.image-container img:first-of-type', imgs => imgs.map((img) => img.getAttribute('src')));
+			const benShermanPrice = await page.$$eval('span.value.bfx-price', get => get.map(ge => ge.getAttribute('content')));
+			const benShermanTitle = await page.$$eval('h3 span.pdp-link', get => get.map(ge => ge.innerText()));
+			output.push({ benShermanPhotos, benShermanPrice, benShermanTitle });
+
+		}
+
+
+
+
+
+		//await page.goto('https://gitlab.com/users/sign_in', { waitUntil: 'networkidle0' });
+
+		//used for testing bad cookies
+		// const cookies = await page.cookies();
+		// const fs = require('fs');
+		// fs.writeFileSync('cookies.json', JSON.stringify(cookies));
+
+		// const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8'));
+		// if (cookies.length > 0) {
+		// 	// Set the cookies to check on the page
+		// 	for (const cookie of cookies) {
+		// 		await page.setCookie(cookie);
+		// 	}
+		// }
+
+		// const sessionCookie = await page.cookies('https://gitlab.com/users/sign_in');
+		// if (sessionCookie) {
+		// 	// Check the expiration date of the cookie
+		// 	const now = new Date();
+		// 	if (!sessionCookie.expires > now.getTime()) {
+		// 		console.log('Cookie is expired');
+		// 		// call login function
+		// 		await page.waitForSelector("#user_login", { visible: true });
+		// 		await page.type("#user_login", process.env.GITUSR);
+		// 		await page.waitForTimeout(111);
+		// 		await page.type("#user_password", process.env.GITPW);
+		// 		await page.waitForTimeout(111);
+		// 		await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), page.click('[data-testid="sign-in-button"]')]);
+
+		// 		const cookies = await page.cookies();
+		// 		const fs = require('fs');
+		// 		fs.writeFileSync('cookies.json', JSON.stringify(cookies));
+
+		// 	} else {
+		// 		console.log('Cookie is current and valid');
+		// 		// Do something with the cookie
+
+		// 	}
+		// }
+
+		// await page.waitForSelector('a[data-nav="login"]');
+		// await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), page.click('a[data-nav="login"]')]);
+
+		//----- login submit
+
+
+
+		//---- goto user activity
+
+		/*
+		
+	
+		//get all ccommit rows
+	
 		let commitRows = await page.$$eval('li.commit > div', pub => pub.map(pu => pu.innerText)); // returns dc017fff · 1|00482 updated for MLR	15068
 		let commitTitles = await page.$$eval('div.commit-row-title a.gfm', tit => tit.map(ti => ti.getAttribute('title')));
 		let issueNo = await page.$$eval('div.commit-row-title a.gfm.gfm-issue', te => te.map(t => t.innerText));
 		let timeStamps = await page.$$eval('div.commit-row-title', time => time.map(tim => tim.parentNode.parentNode.parentNode.parentNode.children[0].innerText)); // Mar 3, 2022, 3:13 PM
 		await page.waitForTimeout(111);
-
+	
 		//log all commit rows if they have the '|' char
 		for (let i = 0; i < commitRows.length; i++) {
 			let dexA = commitRows[i].indexOf("|");
 			let dexB = commitRows[i].indexOf("#");
-
+	
 			let dexPay = commitTitles[i].indexOf("#");
-
+	
 			if (dexA >= 0) {
 				let hrs = commitRows[i].substring(dexA)
 					.replace(/\|\s?iiiii/g, '5')
@@ -59,7 +124,7 @@ clear();
 				let body = commitTitles[i];
 				let payMe = commitTitles[i].substring(dexPay);
 				await page.waitForTimeout(111);
-
+	
 				if (issue == undefined || body == undefined) {
 					issue = commitRows[i].substring(dexB, dexB + 4);
 					body = commitRows[i].substring(dexB + 4, commitRows.length);
@@ -71,14 +136,14 @@ clear();
 				}
 			}
 		}
-
+		*/
 		//BACK AND CLOSE BROWSER
-		await page.waitForTimeout(555);
-		// await browser.close();
-		// process.exit(1);
+
+		await browser.close();
+		process.exit(1);
 	} catch (e) {
 		console.log(`--ERROR--ERROR--ERROR--ERROR\n${e}\nERROR--ERROR--ERROR--ERROR`);
-		// process.exit(1);
+		//process.exit(1);
 	}
 })();
 
