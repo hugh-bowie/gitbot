a09601fdc0b0c35f9c10cafcffa634f555ebe61f
hughbowie <hughbowie.iv@gmail.com>
2023-07-24 13:36:27 -0400

    update the pckg.json

--- inv.js
+++ inv.js
@@ -27,7 +27,7 @@ clear();
 
 		//•••• goto user activity
 		await page.goto('https://gitlab.com/users/' + process.env.GITUSR + '/activity', { waitUntil: 'networkidle2' });
-		for (let i = 0; i < 25; i++) {
+		for (let i = 0; i < 40; i++) {
 			await page.keyboard.press('PageDown');
 			await page.waitForTimeout(100);
 		}
